a70cfba89533652ecace450f0e738937fea59e57
aliya-sh <aliya.shahidd@gmail.com>
2026-06-09 23:50:36 +0500

    Fix server.js - remove broken tournaments route

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-const tournamentRoutes = require("./routes/tournaments");
-const leaderboardRoutes = require("./routes/leaderboard");
-const contactRoutes = require("./routes/contacts");
 const authRoutes         = require('./routes/auth');
 const playerRoutes       = require('./routes/players');
 const registrationRoutes = require('./routes/registrations');
@@ -27,9 +24,6 @@ app.use('/players',       playerRoutes);
 app.use('/registrations', registrationRoutes);
 app.use('/transactions',  transactionRoutes);
 app.use('/chat',          chatRoutes);
-app.use("/api/tournaments", tournamentRoutes);
-app.use("/api/leaderboard", leaderboardRoutes);
-app.use("/api/contacts", contactRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
